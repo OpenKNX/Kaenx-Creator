25a2ff2cdb9bd4c43476aed0272a3b1c528406ed
Mike <45664417+thewhobox@users.noreply.github.com>
2024-01-30 12:52:33 +0100

    fixed UI

---
+++ Kaenx.Creator/Converter/DataToImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace Kaenx.Creator.Converter
+{
+    public class DataToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MemoryStream ms = new MemoryStream(value as byte[]);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
